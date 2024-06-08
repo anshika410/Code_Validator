4df0d4b646ca9aee84a64661468348702c0e0734
Anshika Tripathi <120476909+anshika410@users.noreply.github.com>
2024-06-08 18:24:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("validateButton")
+  .addEventListener("click", function () {
+    window.location.href =
+      "https://partyrock.aws/u/AnshikaTripathi/Tj2486Pp_/OpenBookTestCodeValidator";
+  });
